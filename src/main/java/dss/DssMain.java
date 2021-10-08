6232db3b3dbd4e6144564d631886ebad997c3c29
gbod <gabor.bodnar.05@gmail.com>
2021-10-08 11:52:11 +0200

    2nd part of the method + dss-util impl added as dependency

--- src/main/java/dss/DssMain.java
+++ src/main/java/dss/DssMain.java
@@ -1,13 +1,17 @@
 package dss;
 
-import eu.europa.esig.dss.enumerations.SignatureLevel;
+import eu.europa.esig.dss.enumerations.*;
 import eu.europa.esig.dss.model.*;
 import eu.europa.esig.dss.pades.*;
+import eu.europa.esig.dss.pades.signature.PAdESService;
+import eu.europa.esig.dss.pdf.pdfbox.PdfBoxNativeObjectFactory;
 import eu.europa.esig.dss.token.AbstractKeyStoreTokenConnection;
 import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
 import eu.europa.esig.dss.token.KeyStoreSignatureTokenConnection;
 import eu.europa.esig.dss.token.SignatureTokenConnection;
+import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.security.*;
@@ -47,7 +51,7 @@ class DssMain {
             // Initialize visual signature and configure
             SignatureImageParameters imageParameters = new SignatureImageParameters();
             // set an image
-            imageParameters.setImage(new InMemoryDocument(getClass().getResourceAsStream("/signature-pen.png")));
+//            imageParameters.setImage(new InMemoryDocument(getClass().getResourceAsStream("signature-pen.png")));
             // the origin is the left and top corner of the page
             imageParameters.setxAxis(200);
             imageParameters.setyAxis(400);
@@ -55,10 +59,55 @@ class DssMain {
             imageParameters.setHeight(200);
 
             // Initialize text to generate for visual signature
-            DSSFont font = new DSSFileFont(getClass().getResourceAsStream("OpenSansRegular.ttf"));
+//            DSSFont font = new DSSFileFont(getClass().getResourceAsStream("OpenSansRegular.ttf"));
+
+            // Instantiates a SignatureImageTextParameters object
+            SignatureImageTextParameters textParameters = new SignatureImageTextParameters();
+            // Allows you to set a DSSFont object that defines the text style (see more information in the section "Fonts usage")
+//            textParameters.setFont(font);
+            // Defines the text content
+            textParameters.setText("My visual signature \n #1");
+            // Defines the color of the characters
+            textParameters.setTextColor(Color.BLUE);
+            // Defines the background color for the area filled out by the text
+            textParameters.setBackgroundColor(Color.YELLOW);
+            // Defines a padding between the text and a border of its bounding area
+            textParameters.setPadding(20);
+            // Set textParameters to a SignatureImageParameters object
+            imageParameters.setTextParameters(textParameters);
+
+            // Specifies a text position relatively to an image (Note: applicable only for joint image+text visible signatures).
+            // Thus with _SignerPosition.LEFT_ value, the text will be placed on the left side,
+            // and image will be aligned to the right side inside the signature field
+            textParameters.setSignerTextPosition(SignerTextPosition.LEFT);
+            // Specifies a horizontal alignment of a text with respect to its area
+            textParameters.setSignerTextHorizontalAlignment(SignerTextHorizontalAlignment.RIGHT);
+            // Specifies a vertical alignment of a text block with respect to a signature field area
+            textParameters.setSignerTextVerticalAlignment(SignerTextVerticalAlignment.TOP);
+            parameters.setImageParameters(imageParameters);
 
 
+            // Create common certificate verifier
+            CommonCertificateVerifier commonCertificateVerifier = new CommonCertificateVerifier();
 
+            // Create PAdESService for signature
+            PAdESService service = new PAdESService(commonCertificateVerifier);
+
+            service.setPdfObjFactory(new PdfBoxNativeObjectFactory());
+
+            // Get the SignedInfo segment that need to be signed.
+            ToBeSigned dataToSign = service.getDataToSign(toSignDocument, parameters);
+
+
+            // This function obtains the signature value for signed information using the
+            // private key and specified algorithm
+            DigestAlgorithm digestAlgorithm = parameters.getDigestAlgorithm();
+            SignatureValue signatureValue = signingToken.sign(dataToSign, digestAlgorithm, privateKey);
+
+            // We invoke the xadesService to sign the document with the signature value obtained in the previous step.
+            DSSDocument signedDocument = service.signDocument(toSignDocument, parameters, signatureValue);
+
+            signedDocument.save("D:/tmp/signedByDSS_visible_signature.pdf");
         }
     }
 
